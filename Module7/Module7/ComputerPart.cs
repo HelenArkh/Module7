89778881739eb2782154c500cc070c71b28750fa
HelenArkh <alena.arkh@gmail.com>
2021-01-07 13:45:10 +0300

    Абстрактные классы

---
+++ Module7/Module7/ComputerPart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module7
+{
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+
+    class Processor : ComputerPart
+    {
+        public override void Work() { }
+    }
+
+    class MotherBoard : ComputerPart
+    {
+        public override void Work() { }
+    }
+
+    class GraphicCard : ComputerPart
+    {
+        public override void Work() { }
+    }
+}
---
+++ Module7/Module7/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module7
+{
+    abstract class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public void DisplayName()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Employee1 : Person
+    {
+        // Булевый флаг, сообщающий, находится ли сотрудник на смене
+        public bool IsOnShift;
+
+        public Employee1(string name, bool isOnShift) : base(name)
+        {
+            IsOnShift = isOnShift;
+        }
+    }
+
+    class Guest : Person
+    {
+        // Дата и время прибытия гостя
+        public DateTime ArrivalDate;
+
+        public Guest(string name, DateTime arrivalDate) : base(name)
+        {
+            ArrivalDate = arrivalDate;
+        }
+    }
+}
